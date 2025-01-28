3ef8ee73cdc252101eb0a2242c1847e56de8502f
Darren Raines <draines.123123@gmail.com>
2025-01-28 10:15:14 -0600

    addded welcome page after login

---
+++ src/components/welcome/Welcome.jsx
@@ -0,0 +1,13 @@
+import "./Welcome.css"
+
+export const Welcome = () => {
+    return (
+      <div className="welcome-container">
+        <h1>
+          <span>Meal Prep</span>
+        </h1>
+        <div>Affordable weekly meals</div>
+      </div>
+    );
+  };
+  
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Outlet, Route, Routes } from "react-router-dom"
 import { NavBar } from "../components/navbar/NavBar"
+import { Welcome } from "../components/welcome/Welcome"
 
 
 
@@ -17,9 +18,8 @@ export const ApplicationViews = () => {
           <Outlet />
         </>
       }>
-        <Route index element={<>Home</>} />
-
-        <Route path="employees">
+        <Route index element={<Welcome />} />
+          <Route path="employees">
           <Route index element={<>Employee List</>} />
           <Route path=":employeeId" element={<>Employee Details</>} />
           <Route path="edit/:employeeId" element={<>Edit Employee Details</>} />
