c1e348e85e7f77b341ba2be7ef2c0db1699c5fba
Darren Raines <draines.123123@gmail.com>
2025-01-30 14:40:30 -0600

    Revert "Revert 16 ft/employee details dr"

---
+++ src/components/employees/EmployeeList.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import { getAllUser } from "../../services/employeeService";
+import { useState, useEffect } from "react";
+import "./Employee.css"
+
+export const EmployeeList = () => {
+  const [Admin, setAdmin] = useState([]);
+
+  useEffect(() => {
+    getAllUser().then((userArray) => {
+      setAdmin(userArray);
+    });
+  }, []);
+
+  return (
+    <div className="users">
+      {Admin.map((u) => (
+        <Link className="all-users" key={u.id} to={`/employee/${u.id}`}>
+         <div className="user">
+          <section className="user-Header">{u.fullName}</section>
+          <section className="user-info">{u.email}</section>
+          <section className="user-info">{u.isAdmin}</section>
+         </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
